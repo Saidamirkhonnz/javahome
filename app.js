b95ade12f3022647bdb1daf9052c3178c2e885be
said <322@.ru>
2023-09-24 09:25:44 +0500

    java

---
+++ app.js
@@ -0,0 +1,26 @@
+// let a = +prompt('Введите число')
+// let b = +prompt('Введите степень')
+// let c =1
+// for( let i = 0; i< b; i++){
+//     c = c*a
+// }
+// alert(c);
+
+// let a = prompt('Введите кол во ступенек')
+// let b = prompt('Введите символ')
+// let c = prompt('Введите конечный символ')
+// /* let aw = '' */
+// for(i = 0 ; i < a ; i++){
+
+//     c = i == 0 ? c: b+c
+//     console.log(c);
+//     /*     if (i == 0) {
+//         aw = c
+//     }else{
+
+// aw = aw+b
+
+//     }
+//  */
+// }
+
